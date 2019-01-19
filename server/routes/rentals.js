34ee814b9ab33c31a59c44f50063002668952344
mukeshjha <mukeshjha2007@gmail.com>
2019-01-19 18:53:36 +0530

    create rental server part

--- server/routes/rentals.js
+++ server/routes/rentals.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const Rental = require('../models/rental');
-const UserCtrl = require('../controllers/user')
+const UserCtrl = require('../controllers/user');
+const User = require('../models/user');
 
 
 router.get('/secret',UserCtrl.authMiddleware,function(req,res){
@@ -13,20 +14,43 @@ router.get('/secret',UserCtrl.authMiddleware,function(req,res){
 
 
 
-router.get('',function(req,res){
-     //res.json({'ok':true});
-
-     Rental.find({})
-      .select('-bookings')
-      .exec(function(err,foundRentals){
-      if(err){
-          return res.status(422).send({error:[{title:'Rental Error!',deatial:'Could not find rental'}]});
+router.get('', function(req, res) {
+    const city = req.query.city;
+    const query = city ? {city: city.toLowerCase()} : {};
+   
+    Rental.find(query)
+        .select('-bookings')
+        .exec(function(err, foundRentals) {
+  
+      if (err) {
+        return res.status(422).send({errors: normalizeErrors(err.errors)});
       }
-      return res.json(foundRentals);         
-   });
+  
+      if (city && foundRentals.length === 0) {
+        return res.status(422).send({errors: [{title: 'No Rentals Found!', detail: `There are no rentals for city ${city}`}]});
+      }
+  
+      return res.json(foundRentals);
+    });
+  });
 
-
- });
+router.post('', UserCtrl.authMiddleware, function(req, res) {
+    const { title, city, street, category, image, shared, bedrooms, description, dailyRate } = req.body;
+    const user = res.locals.user;
+  
+    const rental = new Rental({title, city, street, category, image, shared, bedrooms, description, dailyRate});
+    rental.user = user;
+  
+    Rental.create(rental, function(err, newRental) {
+      if (err) {
+        return res.status(422).send({errors: normalizeErrors(err.errors)});
+      }
+  
+      User.update({_id: user.id}, { $push: {rentals: newRental}}, function(){});
+  
+      return res.json(newRental);
+    });
+  }); 
 
  router.get('/:id',function(req,res){
      const rentalId= req.params.id;
